399bc9d2958b1f921efdb9442072de991040cabd
ZeenatFirdosh <zeenatzfq052@gmail.com>
2023-12-25 20:12:19 +0530

    role tried to check

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -19,10 +19,10 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 
 exports.authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
+    if (!roles.includes(req?.user?.role)) {
       return next(
         new ErrorHander(
-          `Role: ${req.user.role} is not allowed to access this resouce `,
+          `Role: ${req.user?.role} is not allowed to access this resouce `,
           403
         )
       );
